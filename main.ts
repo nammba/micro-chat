addce56c0d750e1c4d1bcae39fd5ab8456882371
nammba <pvitulano@gmail.com>
2022-10-15 11:43:40 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-input.onButtonPressed(Button.AB, function () {
-    radio.sendString("I love you papa")
-})
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-})
-basic.clearScreen()
-radio.setGroup(1)
+
